032b1d8b3e4dafbc7286d5b27479026770d3bbe8
Eli Summers <40410999+NaNe3@users.noreply.github.com>
2026-04-20 20:17:29 -0600

    finished ponder 2

---
+++ ponder/variables-constants.js
@@ -0,0 +1,30 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = PI * radius ** 2;
+console.log(area);
+
+radius = 20;
+area = PI * radius ** 2;
+console.log(area);
+
+// type coercion
+const one = 1;
+const two = "2";
+
+let result = one * two;
+console.log(result);
+
+result = one + Number(two);
+console.log(result);
+
+// Scope
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "Eli";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
